058b860d12627c41053bdd682fa886185cf674fe
Dee Schaedler <deeschaedler@gmail.com>
2020-12-10 18:05:57 -0500

    Petrified Lofg Spawning Petrified Logs now Spawn between layers 5 and 32. Confirmed they burn in Lava as expected.

--- src/main/java/com/dschaedler/minebetter/MineBetter.java
+++ src/main/java/com/dschaedler/minebetter/MineBetter.java
@@ -1,20 +1,48 @@
 package com.dschaedler.minebetter;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.Material;
 import net.minecraft.block.PillarBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.BuiltinRegistries;
 import net.minecraft.util.registry.Registry;
+import net.minecraft.util.registry.RegistryKey;
+import net.minecraft.world.gen.GenerationStep;
+import net.minecraft.world.gen.decorator.Decorator;
+import net.minecraft.world.gen.decorator.RangeDecoratorConfig;
+import net.minecraft.world.gen.feature.ConfiguredFeature;
+import net.minecraft.world.gen.feature.Feature;
+import net.minecraft.world.gen.feature.OreFeatureConfig;
+import net.minecraft.world.gen.stateprovider.PillarBlockStateProvider;
 
 public class MineBetter implements ModInitializer {
 
 	// Initialize Blocks
+	// Petrified Log
 	public static final PillarBlock PETRIFIED_LOG = new PillarBlock(FabricBlockSettings.of(Material.WOOD).hardness(2.0f));
-	
+
+	// ConfiguredFeature for Spawning Petrified Logs
+	private static ConfiguredFeature<?, ?> ORE_PETRIFIED_LOG = Feature.ORE
+	.configure(new OreFeatureConfig (
+		OreFeatureConfig.Rules.BASE_STONE_OVERWORLD,
+		PETRIFIED_LOG.getDefaultState(),
+		5)) // Vein Size
+		.decorate(Decorator.RANGE.configure(new RangeDecoratorConfig(
+			0, // bottomOffset, 
+			5, // topOffset,
+			32 // maximum
+			)))
+			.spreadHorizontally()
+			.repeat(10); //Veins per Chunk
+
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
@@ -23,7 +51,16 @@ public class MineBetter implements ModInitializer {
 
 		// Register Blocks
 		Registry.register(Registry.BLOCK, new Identifier("minebetter", "petrified_log"), PETRIFIED_LOG);
-        Registry.register(Registry.ITEM, new Identifier("minebetter", "petrified_log"), new BlockItem(PETRIFIED_LOG, new Item.Settings().group(ItemGroup.MISC)));
+		Registry.register(Registry.ITEM, new Identifier("minebetter", "petrified_log"), new BlockItem(PETRIFIED_LOG, new Item.Settings().group(ItemGroup.MISC)));
+		
+		// Spawn Petrified Logs with Configured Feature
+		// Register the ConfiguredFeature
+		RegistryKey<ConfiguredFeature<?, ?>> orePetrifiedLog = RegistryKey.of(Registry.CONFIGURED_FEATURE_WORLDGEN,
+			new Identifier("minebetter", "ore_petrified_log"));
+		Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, orePetrifiedLog.getValue(), ORE_PETRIFIED_LOG);
+
+		//Spawn Logs
+		BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(), GenerationStep.Feature.UNDERGROUND_ORES, orePetrifiedLog);
 
 	}
 }
