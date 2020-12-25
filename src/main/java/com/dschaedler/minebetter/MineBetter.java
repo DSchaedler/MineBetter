9795863d0e59032c1b57291c62944699f4c42b7b
Dee Schaedler <deeschaedler@gmail.com>
2020-12-25 06:27:35 -0500

    Made Glass Trapdoors transparent. Removed useless println statements.

--- src/main/java/com/dschaedler/minebetter/MineBetter.java
+++ src/main/java/com/dschaedler/minebetter/MineBetter.java
@@ -3,9 +3,11 @@ package com.dschaedler.minebetter;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
 import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Material;
 import net.minecraft.block.PillarBlock;
+import net.minecraft.client.render.RenderLayer;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
@@ -29,9 +31,10 @@ public class MineBetter implements ModInitializer {
 	public static final PillarBlock PETRIFIED_LOG = new PillarBlock(
 		FabricBlockSettings.of(Material.WOOD).hardness(2.0f));
 
+	// Glass Trapdoor
 	public static final MBTrapdoor GLASS_TRAPDOOR_BLOCK = new MBTrapdoor(
 		FabricBlockSettings.of(Material.GLASS).hardness(0.3f));
-	
+
 	// ConfiguredFeature for Spawning Petrified Logs
 	private static ConfiguredFeature<?, ?> ORE_PETRIFIED_LOG = Feature.ORE
 			.configure(new OreFeatureConfig(OreFeatureConfig.Rules.BASE_STONE_OVERWORLD,
@@ -51,25 +54,23 @@ public class MineBetter implements ModInitializer {
 		System.out.println("[MineBetter] Hello from MineBetter!");
 		
 		// Register Blocks
-		System.out.println("[MineBetter] Registering Blocks...");
 		Registry.register(Registry.BLOCK, new Identifier("minebetter", "petrified_log"), PETRIFIED_LOG);
 		Registry.register(Registry.ITEM, new Identifier("minebetter", "petrified_log"), new BlockItem(PETRIFIED_LOG, new Item.Settings().group(ItemGroup.MISC)));
-
 
 		Registry.register(Registry.BLOCK, new Identifier("minebetter", "glass_trapdoor"), GLASS_TRAPDOOR_BLOCK);
 		Registry.register(Registry.ITEM, new Identifier("minebetter", "glass_trapdoor"), new BlockItem(GLASS_TRAPDOOR_BLOCK, new Item.Settings().group(ItemGroup.MISC)));
 
+		// Make the Glass Trapdoor transparent
+		// This works for now, but lets make sure this ends up client side only, yeah?
+		BlockRenderLayerMap.INSTANCE.putBlock(MineBetter.GLASS_TRAPDOOR_BLOCK, RenderLayer.getCutout());
+
 		// Spawn Petrified Logs with Configured Feature
 		// Register the ConfiguredFeature
-		System.out.println("[MineBetter] Spawning Petrified Logs...");
 		RegistryKey<ConfiguredFeature<?, ?>> orePetrifiedLog = RegistryKey.of(Registry.CONFIGURED_FEATURE_WORLDGEN,
 			new Identifier("minebetter", "ore_petrified_log"));
 		Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, orePetrifiedLog.getValue(), ORE_PETRIFIED_LOG);
 
 		//Spawn Logs
 		BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(), GenerationStep.Feature.UNDERGROUND_ORES, orePetrifiedLog);
-
-		System.out.println("[MineBetter] All Done!");
-
 	}
 }
